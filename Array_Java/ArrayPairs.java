c93e1d9aa6bc79427172bf7550908fe6b8818c34
Sayan Mitra <sayan.mitra162@gmail.com>
2023-03-20 23:07:32 +0530

    Printing pairs in Array

---
+++ Array_Java/ArrayPairs.java
@@ -0,0 +1,32 @@
+public class ArrayPairs {
+    
+    public static void printPairs(int arr[]) {
+        
+        //outer loop for traversing the array one element at a time
+        for(int i=0; i<arr.length; i++) {
+            int current = arr[i]; 
+            //inner loop for pairing the current i'th element with the next adjacent elements upto n
+            for(int j=i+1; j<arr.length; j++) {
+                System.out.print("(" + current + " " + arr[j] + ")" + " ");
+            }
+            //for new line
+            System.out.println();
+        }
+    }
+
+    /* 
+    output
+     (2 4) (2 6) (2 8) (2 10) 
+    (4 6) (4 8) (4 10)
+    (6 8) (6 10)
+    (8 10)
+     */
+
+
+
+    public static void main(String args[]) {
+        int list[] = {2,4,6,8,10};
+
+        printPairs(list);
+    }
+}
