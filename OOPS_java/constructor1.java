e20c67536041c0a98dc5a6da954d91d4df44e43c
Sayan Mitra <sayan.mitra162@gmail.com>
2023-07-31 17:32:37 +0530

    new

--- OOPS_java/constructor1.java
+++ OOPS_java/constructor1.java
@@ -1,17 +1,35 @@
 public class constructor1 {
     public static void main(String args[]) {
+        Student s2 = new Student();
+        Student s3 = new Student("Sayan");
+        Student s4 = new Student(27);
         Student s1 = new Student("Sayan Mitra", 27);
-        System.out.println(s1.name + " " + s1.rollNo);
     }
 }
 
  class Student {
-   public String name;
-   public int rollNo;
+     String name;
+     int rollNo;
 
+
+   //non-parametrized - default constructor
+   Student() {
+    System.out.println("this a default, non parameterized constructor");
+   }
+   //parameterized constructor
     Student(String name, int rollNo) {
         this.name = name;
         this.rollNo = rollNo;
-        System.out.println("Constructor is called!");
+        System.out.println("Constructor is called!/parametrized");
+    }
+    
+    Student(String name) {
+        this.name = name;
+    }
+
+    Student(int rollNo) {
+        this.rollNo = rollNo;
     }
 }
+
+
