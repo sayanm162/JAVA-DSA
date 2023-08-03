dc30dd3e9e2e9d523125d581e9b236e550151c2e
Sayan Mitra <sayan.mitra162@gmail.com>
2023-08-03 17:44:05 +0530

    new

--- Bit_Manipulation/EvenOdd.java
+++ Bit_Manipulation/EvenOdd.java
@@ -12,9 +12,28 @@ public class EvenOdd {
             System.out.println("Odd Number");
         }
     }
+
+    public static void getIthBit(int n, int i) {
+        int bitmask = 1<<i;
+        if((n&bitmask)==0) {
+            System.out.println(0);
+
+        }
+        else {
+            System.out.println(1);
+        }
+    }
+
+    public static int setIthBit(int n, int i) {
+        int bitmask=1<<i;
+        return n | bitmask;
+    }
     public static void main(String args[]) {
         evenOrOdd(1);
         evenOrOdd(4);
         evenOrOdd(11);
+
+        getIthBit(10, 3);
+        System.out.println(setIthBit(10, 2));
     }
 }
