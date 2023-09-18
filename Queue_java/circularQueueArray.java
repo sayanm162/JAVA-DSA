de38140a56c59c654640c8f5423df2dee04d5a5a
Sayan Mitra <sayan.mitra162@gmail.com>
2023-09-19 02:56:29 +0530

    new++

---
+++ Queue_java/circularQueueArray.java
@@ -0,0 +1,85 @@
+
+
+public class circularQueueArray {
+
+    static class Queue {
+       static int arr[];
+       static int size;
+       static int rear;
+       static int front;
+        Queue(int n) {
+            arr = new int[n];
+            size = n;
+            rear = -1;
+            front = -1;
+        }
+
+        public static boolean isEmpty() {
+            return rear == -1 && front == -1;
+        }
+
+        //circular queue full
+        public static boolean isFull() {
+            return (rear+1)%size == front;  //indicates next index is again front hence queue filled
+        }
+
+        //add
+        public static void add(int data) {
+            if(isFull()) {
+                System.out.println("Queue is full.");
+                return;
+            }
+            if(front==-1) {
+                 front = (front +1)%size;
+            }
+           
+            rear = (rear + 1)%size;
+            arr[rear] = data;
+        }
+
+        //remove
+        public static int remove() {
+            if(isEmpty()) {
+                System.out.println("Queue Empty");
+                return -1;
+            }
+
+            int top = arr[front];
+            if(front == rear) {
+                front = rear = -1;
+            } 
+            else {
+                front = (front+1)%size;
+            }
+            return top;
+        
+        }
+
+        public static int peek() {
+            if(isEmpty()) {
+                System.out.println("Queue Empty");
+                return -1;
+            }
+
+            int top = arr[front];
+            return front;
+        }
+    }
+
+    public static void main(String args[]) {
+        Queue q = new Queue(3);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        System.out.println(q.remove());
+        q.add(4);
+        System.out.println(q.remove());
+        q.add(5);
+
+        while(!q.isEmpty()) {
+            System.out.println(q.remove());
+        }
+
+    }
+    
+}
