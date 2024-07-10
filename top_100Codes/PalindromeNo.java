5ca096cebfcbb6b75fa14c188048924bb01b80e7
Sayan Mitra <sayan.mitra162@gmail.com>
2024-07-11 00:26:07 +0530

    new

---
+++ top_100Codes/PalindromeNo.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class PalindromeNo {
+
+
+  public static boolean checkPalindrome(int num) {
+      int temp = num;
+      int revNum = 0;
+      while(temp!=0) {
+        int rem = temp%10;
+        revNum = revNum*10 + rem;
+        temp /= 10;
+
+      }
+
+      return num == revNum;
+  }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    System.out.println(checkPalindrome(num));
+
+    sc.close();
+  }
+}
