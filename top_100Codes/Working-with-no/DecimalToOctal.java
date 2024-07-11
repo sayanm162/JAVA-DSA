fd23e80549778f69b5aad012828f6c59efed399a
Sayan Mitra <sayan.mitra162@gmail.com>
2024-07-12 02:40:52 +0530

    new addition

--- top_100Codes/Working-with-no/DecimalToOctal.java
+++ top_100Codes/Working-with-no/DecimalToOctal.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Main{
+public class DecimalToOctal{
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int decimal = sc.nextInt();
