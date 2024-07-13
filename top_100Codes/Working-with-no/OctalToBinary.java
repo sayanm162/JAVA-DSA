bbebdd592fcde62bf5ba75ab368a720dcee82520
Sayan Mitra <sayan.mitra162@gmail.com>
2024-07-13 15:23:09 +0530

    add octalToBinary

--- top_100Codes/Working-with-no/OctalToBinary.java
+++ top_100Codes/Working-with-no/OctalToBinary.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Main{
+public class OctalToBinary{
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int octal = sc.nextInt();
